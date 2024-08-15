f5c10f5a7b4ee45d7fb6f0de9ef7e79970249a74
Dewnaka <lithilakavi@gmail.com>
2024-08-15 22:01:50 +0530

    Added Intro structure,classnames,description

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import styles from './App.module.css'
-import Navbar from "./components/Navbar/Navbar.jsx";
-
+import {Navbar} from "./components/Navbar/Navbar.jsx";
+import {Intro} from "./components/Intro/Intro.jsx"
 function App() {
 
   return (
     <div className={styles.App}>
       <Navbar/>
+        <Intro/>
     </div>
   )
 }
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {getImageURL} from "../../utils.js";
+
+import styles from "./Intro.module.css";
 
 const Intro = () => {
     return (
-        <div>
+        <section className={styles.container}>
+            <div className={styles.content}><h1>Hi! I'm Dewnaka</h1>
+            <p className={styles.description}>As a software engineering undergraduate, I possess strong programming,
+                communication, and leadership skills.
+                I’m passionate about software development and staying ahead of industry trends. I strive to create innovative solutions that bridge the gap
+                between technical and non-technical stakeholders. My goal is to contribute to cutting-edge projects and drive positive change through technology.
+                I am currently expanding my expertise by learning full stack development tools.</p>
+            <a href="mailto:lithilakavi@gmail.com" className={styles.contactBtn}>Contact me</a>
+            </div>
+            <img className={styles.DewnakaImg} src={getImageURL("Intro/dewnaka.png")} alt="Dewnaka Image"></img>
 
 
-        </div>
+
+
+
+
+            <div className={styles.topBlur}/>
+            <div className={styles.bottomBlur}/>
+
+
+        </section>
     );
 };
 
