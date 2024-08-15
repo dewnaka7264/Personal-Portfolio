58f64b1fa49227ea2dc5341319a1b81ba00aa3c7
Dewnaka <lithilakavi@gmail.com>
2024-08-15 18:08:04 +0530

    Added Navbar CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import styles from './App.module.css'
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
 
   return (
     <div className={styles.App}>
-      Hello world!
+      <Navbar/>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
+import styles from "./Navbar.module.css";
+import {getImageURL} from "../../utils.js";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className={styles.navbar}>
+            <a className={styles.title} href='/'>
+                Portfolio
+            </a>
+            <div className={styles.menu}>
+                <ul className={styles.menuItems}>
+                    <li>
+                        <a href="about">About</a>
+                    </li>
+                    <li>
+                        <a href="experience">Experience</a>
+                    </li>
+                    <li>
+                        <a href="projects">Project</a>
+                    </li>
+                    <li>
+                        <a href="contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
