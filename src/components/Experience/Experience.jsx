3d77349c9b1c8776b306c54015d9448a9642f104
Dewnaka <lithilakavi@gmail.com>
2024-08-16 21:27:34 +0530

    Added skills images and direcotory

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-
+import skills from "../../Data/skills.json";
+import education from "../../Data/education.json";
+import {getImageURL} from "../../utils.js";
 const Experience = () => {
     return (
-        <div>
+        <section id="experience">
+            <h2> Experience</h2>
+            <div>
+                <div>{
+                    skills.map((skill,id)=>{
+                        return <div key={id}>
+                            <div><img src={getImageURL(skill.imageSrc)} alt={skill.title}/>
+                            </div>
+                            <p>{skill.title}</p>
+                        </div>
+                    })
+                } </div>
+                <ul>
+                    {education.map((educationIn,id)=>{
+                        return <li key={id}>
+                            <img src={getImageURL(educationIn.imageSrc)} alt={`${educationIn.institution}logo`}/>
+                            <div>
+                                <h3>{`${educationIn.institution},${educationIn.degree}`}</h3>
+                                <p>{`${educationIn.startDate}-${educationIn.endDate}`}</p>
 
-        </div>
+                            </div>
+
+                        </li>
+                    })}
+                </ul>
+            </div>
+
+        </section>
     );
 };
 
