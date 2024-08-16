bd0f328bc639720ec0145fc9e1122ea3bcdac712
Dewnaka <lithilakavi@gmail.com>
2024-08-16 10:57:31 +0530

    Added content for about section and job role decription

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import {getImageURL} from "../../utils.js";
 
 const About = () => {
     return (
         <section>
 <h2>About</h2>
+            <div>
+                <img src={getImageURL()} alt="Dewnaka image "/>
+            </div>
+            <ul><li><img src={getImageURL("About/cursor-icon.")} alt="cursor"/>
+                <div>
+                   <h3>Full Stack Developer</h3>
+                    <p>As a Software Engineer, I design, develop, and maintain software applications. My strong programming skills in Java, Python, and JavaScript,
+                        combined with my experience in React and Firebase, allow me to contribute effectively to various software development projects.
+                        I work closely with cross-functional teams to create high-quality software solutions that meet client requirements and enhance user experience.</p>
+                </div>
+            </li>
+                <li><img src={getImageURL("About/web-icon.")} alt="web"/>
+                    <div>
+                        <h3>Software Engineer</h3>
+                        <p>In my role as a Full Stack Developer, I leverage my knowledge of both front-end and back-end technologies to
+                            develop fully functioning web applications. With expertise in React.js for front-end development and Firebase for back-end services,
+                            I build responsive, scalable, and robust applications. I manage the complete software development lifecycle,
+                            from designing the user interface
+                            to managing databases and ensuring smooth deployment using CI/CD pipelines like GitHub Actions.
+                        </p>
+                    </div>
+                </li>
+                <li><img src={getImageURL("About/web-icon.")} alt="web"/>
+                    <div>
+                        <h3> Machine Learning Engineer</h3>
+                        <p>I am well-equipped to pursue a role as a Machine Learning Engineer. I develop and optimize machine learning models,
+                            perform data pre-processing, and interpret complex datasets to extract meaningful insights. My understanding of R and Python is
+                            essential for developing and deploying predictive models in various business applications.
+                        </p>
+                    </div>
+                </li>
+            </ul>
         </section>
     );
 };
